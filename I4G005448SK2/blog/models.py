79db0ca23482179bb70271afe428b79163f9fe0d
Viashima <72166830+clins10@users.noreply.github.com>
2022-06-14 02:42:52 +0100

    first commit-pushed the project

---
+++ I4G005448SK2/blog/models.py
@@ -0,0 +1,12 @@
+from time import time
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(default=datetime.now)
+
+
